d67004b7c237f9239df058a8ac4e62212c1fb88f
MattZK <me@mattwill.be>
2019-10-13 20:50:16 +0200

    Added global get node

--- routes/nodes.route.js
+++ routes/nodes.route.js
@@ -21,6 +21,28 @@ module.exports = (services) => {
   //   }
   // );
 
+  router.get('/:id', [
+    param('id').isMongoId().trim().escape(),
+  ], async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({
+        error: 'Unprocessable Entity',
+        code: 422,
+        reason: 'Invalid fields',
+        detail: errors.array()
+      });
+
+    services.models.node.findOne({ _id: req.params.id, members: req.user.sub }).select('-__v -authorizationKey').populate({ path: 'members', select: '_id firstname lastname' }).exec((error, node) => {
+      if (error || !node)
+        return res.sendStatus(403);
+
+      res.send(node);
+    });
+
+  });
+
   router.put('/:id/actuator', [
     param('id').isMongoId().trim().escape(),
     body('actuator').isIn(services.types.actuators).trim().escape(),
@@ -57,6 +79,6 @@ module.exports = (services) => {
     });
 
   });
-
+  
   return router;
 };
