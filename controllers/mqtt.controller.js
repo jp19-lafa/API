78e046638da04150bca59bd8a9449bab83aa9529
Restyled.io <commits@restyled.io>
2019-10-13 18:55:40 +0000

    Restyled by prettier

--- controllers/mqtt.controller.js
+++ controllers/mqtt.controller.js
@@ -1,85 +1,84 @@
-
-
 module.exports = {
-
-  MqttController : class {
+  MqttController: class {
     constructor(services) {
       this.services = services;
 
-      services.mqtt.authenticate =
-          (client, username, password,
-           callback) => {
-            if (!client.id.match(/^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$/))
+      services.mqtt.authenticate = (client, username, password, callback) => {
+        if (!client.id.match(/^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$/))
+          callback(null, false);
+        services.models.node
+          .findOne({ macAddress: username, authorizationKey: password })
+          .exec((error, node) => {
+            if (!node || error) {
               callback(null, false);
-            services.models.node
-                .findOne({macAddress : username, authorizationKey : password})
-                .exec((error, node) => {
-                  if (!node || error) {
-                    callback(null, false);
-                    services.logger.info(`Client ${client.id} unauthorized.`);
-                  } else {
-                    services.logger.info(`Client ${client.id} authorized.`);
-                    callback(null, true);
-                  }
-                });
-          }
+              services.logger.info(`Client ${client.id} unauthorized.`);
+            } else {
+              services.logger.info(`Client ${client.id} authorized.`);
+              callback(null, true);
+            }
+          });
+      };
 
-                        services.mqtt.on('ready', () => {
-                          services.logger.info('MQTT Server Running');
-                        });
+      services.mqtt.on("ready", () => {
+        services.logger.info("MQTT Server Running");
+      });
 
-      services.mqtt.on('clientDisconnected', function(client) {
+      services.mqtt.on("clientDisconnected", function(client) {
         services.logger.info(`Client ${client.id} disconnected.`);
-        services.models.node.findOne({macAddress : client.id})
-            .exec((error, node) => {
-              if (!node || error)
-                return;
+        services.models.node
+          .findOne({ macAddress: client.id })
+          .exec((error, node) => {
+            if (!node || error) return;
 
-              node.status = false
-              node.save();
-            });
+            node.status = false;
+            node.save();
+          });
       });
 
-      services.mqtt.on('clientConnected', function(client) {
+      services.mqtt.on("clientConnected", function(client) {
         services.logger.info(`Client ${client.id} connected.`);
-        services.models.node.findOne({macAddress : client.id})
-            .exec((error, node) => {
-              if (!node || error)
-                return;
+        services.models.node
+          .findOne({ macAddress: client.id })
+          .exec((error, node) => {
+            if (!node || error) return;
 
-              node.status = true
-              node.save();
-            });
+            node.status = true;
+            node.save();
+          });
       });
 
-      services.mqtt.on('published', (packet, client) => {
-        if (packet.topic.split('/')[0].match(
-                /^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$/))
+      services.mqtt.on("published", (packet, client) => {
+        if (
+          packet.topic
+            .split("/")[0]
+            .match(/^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$/)
+        )
           this.handleNodeMessage(packet, client);
       });
     }
 
     // Handle all node related messages
     handleNodeMessage(packet, client) {
-      switch (packet.topic.split('/')[1]) {
-      case 'sensors':
-        this.handleSensorMessage(packet, client);
-        break;
-      case 'actuators':
-        this.handleActuatorMessage(packet, client);
-        break;
+      switch (packet.topic.split("/")[1]) {
+        case "sensors":
+          this.handleSensorMessage(packet, client);
+          break;
+        case "actuators":
+          this.handleActuatorMessage(packet, client);
+          break;
       }
     }
 
     // Handle all node sensor related messages
     handleSensorMessage(packet, client) {
       this.services.logger.info(
-          `Received: [SENSOR] ${packet.payload.toString()} for ${
-              packet.topic.split('/')[2]} on ${client.id}`);
+        `Received: [SENSOR] ${packet.payload.toString()} for ${
+          packet.topic.split("/")[2]
+        } on ${client.id}`
+      );
     }
 
     // Handle all node actuator related messages
     handleActuatorMessage(packet, client) {}
   }
-
-}
+};
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,79 +1,92 @@
-const router = require('express').Router({mergeParams : true});
-const {AuthController} = require('../controllers/auth.controller');
-const {body, validationResult} = require('express-validator');
+const router = require("express").Router({ mergeParams: true });
+const { AuthController } = require("../controllers/auth.controller");
+const { body, validationResult } = require("express-validator");
 
-module.exports = (services) => {
+module.exports = services => {
   const authController = new AuthController(services);
 
   // TODO: Cleanup responses
-  router.post('/login',
-              [
-                body('email').isEmail().trim().escape(),
-                body('password').isLength({min : 5}).trim().escape()
-              ],
-              async (req, res) => {
-                const errors = validationResult(req);
-                if (!errors.isEmpty())
-                  return res.status(422).json({
-                    error : 'Unprocessable Entity',
-                    code : 422,
-                    reason : 'Invalid body fields',
-                    detail : errors.array()
-                  });
+  router.post(
+    "/login",
+    [
+      body("email")
+        .isEmail()
+        .trim()
+        .escape(),
+      body("password")
+        .isLength({ min: 5 })
+        .trim()
+        .escape()
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(422).json({
+          error: "Unprocessable Entity",
+          code: 422,
+          reason: "Invalid body fields",
+          detail: errors.array()
+        });
 
-                const user = await authController
-                                 .authenticateUserByCredentials(
-                                     req.body.email, req.body.password)
-                                 .catch(error => services.logger.error(error));
+      const user = await authController
+        .authenticateUserByCredentials(req.body.email, req.body.password)
+        .catch(error => services.logger.error(error));
 
-                if (!user)
-                  return res.status(403).send({
-                    error : 'Forbidden',
-                    code : 403,
-                    reason : 'Invalid User Credentials'
-                  });
+      if (!user)
+        return res.status(403).send({
+          error: "Forbidden",
+          code: 403,
+          reason: "Invalid User Credentials"
+        });
 
-                const tokenSet =
-                    await authController.generateTokenSet(user).catch(
-                        error => services.logger.error(error));
+      const tokenSet = await authController
+        .generateTokenSet(user)
+        .catch(error => services.logger.error(error));
 
-                return res.send(tokenSet);
-              });
+      return res.send(tokenSet);
+    }
+  );
 
-  router.post('/refresh',
-              [
-                body('token').isUUID(4).trim().escape(),
-              ],
-              async (req, res) => {
-                const errors = validationResult(req);
-                if (!errors.isEmpty())
-                  return res.status(422).json({
-                    error : 'Unprocessable Entity',
-                    code : 422,
-                    reason : 'Invalid body fields',
-                    detail : errors.array()
-                  });
+  router.post(
+    "/refresh",
+    [
+      body("token")
+        .isUUID(4)
+        .trim()
+        .escape()
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(422).json({
+          error: "Unprocessable Entity",
+          code: 422,
+          reason: "Invalid body fields",
+          detail: errors.array()
+        });
 
-                const user = await authController
-                                 .authenticateUserByRefreshToken(req.body.token)
-                                 .catch(error => services.logger.error(error));
+      const user = await authController
+        .authenticateUserByRefreshToken(req.body.token)
+        .catch(error => services.logger.error(error));
 
-                if (!user)
-                  return res.status(403).send({
-                    error : 'Forbidden',
-                    code : 403,
-                    reason : 'Invalid Refresh Token'
-                  });
+      if (!user)
+        return res.status(403).send({
+          error: "Forbidden",
+          code: 403,
+          reason: "Invalid Refresh Token"
+        });
 
-                const tokenSet =
-                    await authController.generateTokenSet(user).catch(
-                        error => services.logger.error(error));
+      const tokenSet = await authController
+        .generateTokenSet(user)
+        .catch(error => services.logger.error(error));
 
-                return res.send(tokenSet);
-              });
+      return res.send(tokenSet);
+    }
+  );
 
-  router.post('/register',
-              async (req, res) => {res.status(418).send('Not Implemented')});
+  router.post("/register", async (req, res) => {
+    res.status(418).send("Not Implemented");
+  });
 
   return router;
 };
