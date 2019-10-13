bcdf075ee1ea3aaa65c6b8b68f546a823d6bc0e6
Restyled.io <commits@restyled.io>
2019-10-13 18:57:38 +0000

    Restyled by clang-format

--- models/user.model.js
+++ models/user.model.js
@@ -1,17 +1,14 @@
 const mongoose = require("mongoose");
 
 module.exports = {
-  userSchema: new mongoose.Schema({
-    firstname: { type: String, required: true },
-    lastname: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    enabled: { type: Boolean, default: true },
-    refreshToken: String,
-    passwordReset: {
-      token: { type: String },
-      expires: { type: Date }
-    },
-    joined: { type: Date, default: Date.now }
+  userSchema : new mongoose.Schema({
+    firstname : {type : String, required : true},
+    lastname : {type : String, required : true},
+    email : {type : String, required : true, unique : true},
+    password : {type : String, required : true},
+    enabled : {type : Boolean, default : true},
+    refreshToken : String,
+    passwordReset : {token : {type : String}, expires : {type : Date}},
+    joined : {type : Date, default : Date.now}
   })
 };
