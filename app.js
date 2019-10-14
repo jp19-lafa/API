d4463401788b6c3eba6f9ab4c94eb1c21175c81d
Restyled.io <commits@restyled.io>
2019-10-14 09:15:23 +0000

    Restyled by prettier

--- app.js
+++ app.js
@@ -6,52 +6,56 @@ const cors = require("cors");
 const jwt = require("express-jwt");
 const app = express();
 const fs = require("fs");
-const {MqttController} = require("./controllers/mqtt.controller");
+const { MqttController } = require("./controllers/mqtt.controller");
 
 const logger = winston.createLogger();
 
-logger.add(new winston.transports.Console({format : winston.format.simple()}));
+logger.add(new winston.transports.Console({ format: winston.format.simple() }));
 
 const server = new mosca.Server({
-  port : 1887,
-  backend : {
-    type : "mongo",
-    url : "mongodb://database:27017/mqtt",
-    pubsubCollection : "ascoltatori",
-    mongo : {}
+  port: 1887,
+  backend: {
+    type: "mongo",
+    url: "mongodb://database:27017/mqtt",
+    pubsubCollection: "ascoltatori",
+    mongo: {}
   }
 });
 
-const {userSchema} = require("./models/user.model");
+const { userSchema } = require("./models/user.model");
 const user = database.model("User", userSchema);
 
-const {nodeSchema} = require("./models/node.model");
+const { nodeSchema } = require("./models/node.model");
 const node = database.model("Node", nodeSchema);
 
 // Mongoose Connection
 database
-    .connect("mongodb://database:27017/farmlab", {
-      useNewUrlParser : true,
-      useFindAndModify : false,
-      useUnifiedTopology : true,
-      useCreateIndex : true
-    })
-    .then(() => logger.info("Database Connected"), err => {
+  .connect("mongodb://database:27017/farmlab", {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(
+    () => logger.info("Database Connected"),
+    err => {
       logger.error("Database Connection Error", err);
       process.exit(1);
-    });
+    }
+  );
 
 // CORS
 const whitelist = [
-  "http://localhost:4200", "https://api.farmlab.team",
+  "http://localhost:4200",
+  "https://api.farmlab.team",
   "https://api.staging.farmlab.team"
 ];
 const corsOptionsDelegate = (req, callback) => {
   var corsOptions;
   if (whitelist.indexOf(req.header("Origin")) !== -1) {
-    corsOptions = {origin : true};
+    corsOptions = { origin: true };
   } else {
-    corsOptions = {origin : false};
+    corsOptions = { origin: false };
   }
   callback(null, corsOptions);
 };
@@ -63,25 +67,27 @@ app.use(express.json());
 
 // Define app wide services
 let services = {
-  database : database,
-  mqtt : server,
-  logger : logger,
-  models : {user : user, node : node},
-  keys : {
-    private : fs.readFileSync("keys/private.key"),
-    public : fs.readFileSync("keys/public.key")
+  database: database,
+  mqtt: server,
+  logger: logger,
+  models: { user: user, node: node },
+  keys: {
+    private: fs.readFileSync("keys/private.key"),
+    public: fs.readFileSync("keys/public.key")
   },
-  types : {
-    actuators : [ "lightint", "flowpump", "foodpump" ],
-    sensors : [ "airtemp", "watertemp", "lightstr", "airhumidity", "waterph" ]
+  types: {
+    actuators: ["lightint", "flowpump", "foodpump"],
+    sensors: ["airtemp", "watertemp", "lightstr", "airhumidity", "waterph"]
   }
 };
 
 let mqttController = new MqttController(services);
 
-app.use(jwt({secret : services.keys.private}).unless({
-  path : [ "/auth/login", "/auth/refresh" ]
-}));
+app.use(
+  jwt({ secret: services.keys.private }).unless({
+    path: ["/auth/login", "/auth/refresh"]
+  })
+);
 
 // Routes
 app.use("/auth", require("./routes/auth.route")(services));
@@ -89,11 +95,14 @@ app.use("/nodes", require("./routes/nodes.route")(services));
 
 app.use(function(err, req, res, next) {
   if (err.name === "UnauthorizedError") {
-    res.status(401).send(
-        {error : "Unautorized", code : 401, reason : "Invalid Token"});
+    res
+      .status(401)
+      .send({ error: "Unautorized", code: 401, reason: "Invalid Token" });
   }
 });
 
-app.listen(8080, "0.0.0.0", () => { logger.info("HTTP Server Running"); });
+app.listen(8080, "0.0.0.0", () => {
+  logger.info("HTTP Server Running");
+});
 
 module.exports = app;
