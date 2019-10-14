f9030f1d4adcd68bca0243bdb391cfe3f3c3842e
Restyled.io <commits@restyled.io>
2019-10-14 09:15:20 +0000

    Restyled by clang-format

--- test/test.js
+++ test/test.js
@@ -2,9 +2,6 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('App', function() {
-  it('Sends 401 on /', function(done) {
-    request(app)
-      .get('/')
-      .expect(401, done);
-  });
-}); 
+  it('Sends 401 on /',
+     function(done) { request(app).get('/').expect(401, done); });
+});
