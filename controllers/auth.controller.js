1421531df9d0e6fccd3a007a744e3db2d038ce19
Restyled.io <commits@restyled.io>
2019-10-19 16:21:20 +0000

    Restyled by clang-format

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,52 +5,43 @@ const jwt = require("jsonwebtoken");
 const hashRounds = process.env.HASH_ROUNDS || 12;
 
 module.exports = {
-  AuthController: class {
+  AuthController : class {
     constructor(services) {
       this.services = services;
-      services.models.user
-        .findOne({ email: "admin@farmlab.team" })
-        .exec((error, user) => {
-          if (!user) {
-            user = new services.models.user({
-              firstname: "Administrator",
-              lastname: "Global",
-              email: "admin@farmlab.team",
-              password: bcrypt.hashSync("IAmAdministrator", hashRounds)
-            });
-            user.save().then(result => {
-              services.logger.info("Created Global Admin");
-            });
-          }
-        });
+      services.models.user.findOne({email : "admin@farmlab.team"})
+          .exec((error, user) => {
+            if (!user) {
+              user = new services.models.user({
+                firstname : "Administrator",
+                lastname : "Global",
+                email : "admin@farmlab.team",
+                password : bcrypt.hashSync("IAmAdministrator", hashRounds)
+              });
+              user.save().then(
+                  result => { services.logger.info("Created Global Admin"); });
+            }
+          });
     }
 
     refreshTokenSet(sub, refreshToken) {}
 
     async authenticateUserByCredentials(email, password) {
       return new Promise((resolve, reject) => {
-        this.services.models.user
-          .findOne({ email: email })
-          .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) resolve(user);
-            resolve(null);
-          })
-          .catch(error => {
-            reject(error);
-          });
+        this.services.models.user.findOne({email : email})
+            .then(user => {
+              if (user && bcrypt.compareSync(password, user.password))
+                resolve(user);
+              resolve(null);
+            })
+            .catch(error => { reject(error); });
       });
     }
 
     async authenticateUserByRefreshToken(token) {
       return new Promise((resolve, reject) => {
-        this.services.models.user
-          .findOne({ refreshToken: token })
-          .then(user => {
-            resolve(user);
-          })
-          .catch(error => {
-            reject(error);
-          });
+        this.services.models.user.findOne({refreshToken : token})
+            .then(user => { resolve(user); })
+            .catch(error => { reject(error); });
       });
     }
 
@@ -58,28 +49,23 @@ module.exports = {
       return new Promise((resolve, reject) => {
         const refreshToken = uuidv4();
         this.services.models.user
-          .findByIdAndUpdate(
-            user._id,
-            { refreshToken: refreshToken },
-            { new: true }
-          )
-          .exec((error, user) => {
-            if (error) reject(error);
+            .findByIdAndUpdate(user._id, {refreshToken : refreshToken},
+                               {new : true})
+            .exec((error, user) => {
+              if (error)
+                reject(error);
 
-            resolve({
-              jwt: this._generateJWT(user._id),
-              refresh: user.refreshToken
+              resolve({
+                jwt : this._generateJWT(user._id),
+                refresh : user.refreshToken
+              });
             });
-          });
       });
     }
 
     _generateJWT(sub, options = {}) {
-      return jwt.sign(
-        { audience: "aud:*", issuer: "FarmLabTeam", sub: sub },
-        this.services.keys.private,
-        { expiresIn: "24h" }
-      );
+      return jwt.sign({audience : "aud:*", issuer : "FarmLabTeam", sub : sub},
+                      this.services.keys.private, {expiresIn : "24h"});
     }
   }
 };
--- routes/nodes.route.js
+++ routes/nodes.route.js
@@ -1,104 +1,86 @@
-const router = require("express").Router({ mergeParams: true });
-const { body, param, validationResult } = require("express-validator");
+const router = require("express").Router({mergeParams : true});
+const {body, param, validationResult} = require("express-validator");
 
 module.exports = services => {
   router.get("/testnode", async (req, res) => {
     services.logger.info("GET /nodes");
 
     let nodeM = new services.models.node({
-      label: "Development Node Alfa",
-      macAddress: "AA:AA:AA:AA:AA:AA",
-      authorizationKey: "76989157-fe00-4d36-87f0-745f8ab73c2d",
-      allowPublicStats: true
+      label : "Development Node Alfa",
+      macAddress : "AA:AA:AA:AA:AA:AA",
+      authorizationKey : "76989157-fe00-4d36-87f0-745f8ab73c2d",
+      allowPublicStats : true
     });
 
-    nodeM.save().then(node => {
-      res.send({ amount: 0, result: node });
-    });
+    nodeM.save().then(node => { res.send({amount : 0, result : node}); });
   });
 
   router.get(
-    "/:id",
-    [
-      param("id")
-        .isMongoId()
-        .trim()
-        .escape()
-    ],
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty())
-        return res.status(422).json({
-          error: "Unprocessable Entity",
-          code: 422,
-          reason: "Invalid fields",
-          detail: errors.array()
-        });
+      "/:id", [ param("id").isMongoId().trim().escape() ], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+          return res.status(422).json({
+            error : "Unprocessable Entity",
+            code : 422,
+            reason : "Invalid fields",
+            detail : errors.array()
+          });
 
-      services.models.node
-        .findOne({ _id: req.params.id, members: req.user.sub })
-        .select("-__v -authorizationKey")
-        .populate({ path: "members", select: "_id firstname lastname" })
-        .exec((error, node) => {
-          if (error || !node) return res.sendStatus(403);
+        services.models.node
+            .findOne({_id : req.params.id, members : req.user.sub})
+            .select("-__v -authorizationKey")
+            .populate({path : "members", select : "_id firstname lastname"})
+            .exec((error, node) => {
+              if (error || !node)
+                return res.sendStatus(403);
 
-          res.send(node);
-        });
-    }
-  );
+              res.send(node);
+            });
+      });
 
-  router.put(
-    "/:id/actuator",
-    [
-      param("id")
-        .isMongoId()
-        .trim()
-        .escape(),
-      body("actuator")
-        .isIn(services.types.actuators)
-        .trim()
-        .escape(),
-      body("state")
-        .isNumeric()
-        .trim()
-        .escape()
-      // .custom(value => {
-      //   console.log(value);
-      //   if (parseInt(value) > 255 || parseInt(value) < 0) return
-      //   Promise.reject('Invalid state value');
-      // })
-    ],
-    async (req, res) => {
-      const errors = validationResult(req);
-      if (!errors.isEmpty())
-        return res.status(422).json({
-          error: "Unprocessable Entity",
-          code: 422,
-          reason: "Invalid fields",
-          detail: errors.array()
-        });
+  router.put("/:id/actuator",
+             [
+               param("id").isMongoId().trim().escape(),
+               body("actuator").isIn(services.types.actuators).trim().escape(),
+               body("state").isNumeric().trim().escape()
+               // .custom(value => {
+               //   console.log(value);
+               //   if (parseInt(value) > 255 || parseInt(value) < 0) return
+               //   Promise.reject('Invalid state value');
+               // })
+             ],
+             async (req, res) => {
+               const errors = validationResult(req);
+               if (!errors.isEmpty())
+                 return res.status(422).json({
+                   error : "Unprocessable Entity",
+                   code : 422,
+                   reason : "Invalid fields",
+                   detail : errors.array()
+                 });
 
-      services.models.node
-        .findOne({ _id: req.params.id, members: req.user.sub })
-        .exec((error, node) => {
-          if (error || !node) return res.sendStatus(403);
+               services.models.node
+                   .findOne({_id : req.params.id, members : req.user.sub})
+                   .exec((error, node) => {
+                     if (error || !node)
+                       return res.sendStatus(403);
 
-          const packet = {
-            topic: `${node.macAddress}/actuators/${req.body.actuator}`,
-            payload: req.body.state,
-            qos: 2,
-            retain: false
-          };
+                     const packet = {
+                       topic :
+                           `${node.macAddress}/actuators/${req.body.actuator}`,
+                       payload : req.body.state,
+                       qos : 2,
+                       retain : false
+                     };
 
-          services.logger.info(
-            `Published: [ACTUATOR] ${req.body.state} for ${req.body.actuator} to ${node.macAddress}`
-          );
-          
-          services.mqtt.publish(packet);
-          res.sendStatus(200);
-        });
-    }
-  );
+                     services.logger.info(
+                         `Published: [ACTUATOR] ${req.body.state} for ${
+                             req.body.actuator} to ${node.macAddress}`);
+
+                     services.mqtt.publish(packet);
+                     res.sendStatus(200);
+                   });
+             });
 
   return router;
 };
